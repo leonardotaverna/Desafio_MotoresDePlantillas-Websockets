b17b3f3ea4936eff5b91fdc413c3db270ac8b814
Leonardo <leonardotaverna@hotmail.com>
2023-08-06 12:00:45 -0300

    inicio configuración websocket

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import __dirname from './utils.js';
 import handlebars from 'express-handlebars';
-import productsRouter from './routes/products.router.js';
-import cartsRouter from './routes/carts.router.js';
+//import productsRouter from './routes/products.router.js';
+//import cartsRouter from './routes/carts.router.js';
 import viewsRouter from './routes/views.router.js';
 import { Server } from 'socket.io'; 
 
@@ -10,18 +10,24 @@ const app = express();
 
 app.use (express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', handlebars.engine());
-app.set('views', __dirname+'/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 
 app.use('/api/views',viewsRouter);
-
 
 const PORT = 8080
 const httpServer = app.listen(PORT,()=>{
     console.log(`Escuchando al puerto ${PORT}`);
 });
 
-const io = new Server (httpServer);
+const socketServer = new Server (httpServer);
+
+socketServer.on ('connection', socket =>{
+    console.log("Nuevo cliente conectado");
+    socket.on('message', data => {
+        console.log(data);
+    })
+});
