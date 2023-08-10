c15f7c4fbc08801fa8a85bcc0f2bc0398577b80a
leonardotaverna <leonardotaverna@hotmail.com>
2023-08-10 20:27:28 -0300

    cambio menor

--- src/ProductsManager.js
+++ src/ProductsManager.js
@@ -42,7 +42,7 @@ class ProductsManager {
                 title,
                 description,
                 price,
-                //thumbnail,
+                thumbnail,
                 code,
                 stock,
             };
